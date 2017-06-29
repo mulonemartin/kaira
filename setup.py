6a0d8416c90836053406950a064151eb9304e230
mulonemartin <mulone.martin@gmail.com>
2017-06-29 09:49:01 -0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='kaira',
+    version='0.1',
+    packages=['kaira', 'kaira.routing'],
+    url='https://github.com/mulonemartin/kaira/',
+    license='MIT',
+    author='martin',
+    author_email='mulone.martin@gmail.com',
+    description='Web Microframework - Python'
+)
