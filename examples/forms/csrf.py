74cb02f201568cc6c0a92805d8c0d01d18eaf16f
mulonemartin <mulone.martin@gmail.com>
2018-04-16 16:27:46 -0300

    forms wtf

---
+++ examples/forms/csrf.py
@@ -0,0 +1,32 @@
+from wtforms import Form, BooleanField, StringField, validators
+
+
+from kaira.app import App
+from kaira.response import response
+from kaira.wtf import KairaForm
+
+
+app = App()
+
+
+class LoginForm(KairaForm):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = StringField('Password', [validators.Length(min=6, max=35)])
+
+
+@app.route("/", methods=['GET', 'POST'])
+def form_validation(request):
+    form = LoginForm(request)
+    if form.validate_on_submit():
+        return response.redirect('/good')
+
+    return response.template('csrf.html', cookies=form.meta.cookies, form=form)
+
+
+@app.route("/good")
+def form_good(request):
+    return response.text('Good!')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=8000)
--- kaira/wtf.py
+++ kaira/wtf.py
@@ -1,21 +1,33 @@
 import base64
 import pickle
+from datetime import timedelta
+import datetime
+import hmac
+from hashlib import sha1
+import os
+import hashlib
 
 from wtforms import Form
 from wtforms.widgets import HiddenInput
+from wtforms.csrf.session import SessionCSRF
+from wtforms.csrf.core import CSRF
+from wtforms import ValidationError
 
 from multidict import CIMultiDict
+
+from kaira.cookie import CookieManager
 
 
 SUBMIT_METHODS = ('POST', 'PUT', 'PATCH', 'DELETE')
 
 
-class KairaForm(Form):
+class KairaFormBase(Form):
 
     def __init__(self, request, **kwargs):
 
         self.request = request
 
+        self.form_name = 'kaira_form_'
         input_vars = dict()
         if request.method == 'GET':
             input_method = request.query
@@ -30,6 +42,7 @@ class KairaForm(Form):
         input_vars = CIMultiDict(**input_vars)
 
         super().__init__(formdata=input_vars, **kwargs)
+        #self.generate_form_name()  # Generamos el nombre del form (uuid)
 
     def is_submitted(self):
         """Consider the form submitted if there is an active request and
@@ -71,11 +84,24 @@ class KairaForm(Form):
 
         return u'\n'.join(str(f) for f in hidden_fields(fields or self))
 
+    def generate_form_name(self):
+        """Generamos el id"""
+
+        l_fields = list()
+        for f in self:
+            l_fields.append(f.id)
+        sums_ids = ''.join(l_fields)
+        hash_code = hashlib.sha256(sums_ids.encode('utf-8')).hexdigest()
+
+        self.form_name = 'kaira_form_' + hash_code
+
     def render_boostrap_fields(self):
         """ Render fields"""
 
         xml_form = list()
         for field in self:
+            if field.id == 'csrf_token':  # salteamos el csrf token
+                continue
             xml_field = list()
             xml_field.append('%s' % field.label)
             xml_field.append('%s' % field(class_='form-control'))
@@ -94,6 +120,194 @@ class KairaForm(Form):
         submit = '<button type="submit"> Send </button>'
         xml = '<form method="POST" enctype="multipart/form-data" action="{path}">{fields} {submit}</form>'\
             .format(path=path, fields=r_fields, submit=submit)
+        return xml
+
+
+class KSessionCSRF(CSRF):
+    TIME_FORMAT = '%Y%m%d%H%M%S'
+
+    def setup_form(self, form):
+
+        self.form_meta = form.meta
+        return super(KSessionCSRF, self).setup_form(form)
+
+    def create_session(self):
+
+        meta = self.form_meta
+        expire_seconds = int(meta.csrf_options['CSRF_COOKIE_EXPIRE'])
+        if expire_seconds > 0:
+            now = datetime.datetime.utcnow()
+            expires_cookie = now + datetime.timedelta(seconds=expire_seconds)
+        else:
+            expires_cookie = None
+
+        if not meta.csrf_options['CSRF_COOKIE_DOMAIN'] or meta.csrf_options['CSRF_COOKIE_DOMAIN'] == "":
+            domain = meta.request.host.split(':')[0]
+        else:
+            domain = meta.csrf_options['CSRF_COOKIE_DOMAIN']
+
+        cookies_options = {
+            'HTTP_COOKIE_DOMAIN': domain,
+            'HTTP_COOKIE_SECURE': meta.csrf_options['CSRF_COOKIE_SECURE'],
+            'HTTP_COOKIE_HTTPONLY': meta.csrf_options['CSRF_COOKIE_HTTPONLY']
+        }
+
+        cookie_name = meta.csrf_options['CSRF_COOKIE_NAME'] + "_" + meta.form_name
+        time_limit = meta.csrf_options['CSRF_TIME_LIMIT']
+        csrf_secret = meta.csrf_options['CSRF_SECRET']
+
+        cookies = meta.cookies
+        if not cookies:
+            cookies = CookieManager(options=cookies_options)
+
+        rand_code = sha1(os.urandom(64)).hexdigest()
+
+        if time_limit:
+            expires = (self.now() + time_limit).strftime(self.TIME_FORMAT)
+            csrf_build = '%s%s' % (rand_code, expires)
+        else:
+            expires = ''
+            csrf_build = rand_code
+
+        hmac_csrf = hmac.new(csrf_secret, csrf_build.encode('utf8'), digestmod=sha1)
+        value_csrf = '%s##%s' % (expires, hmac_csrf.hexdigest())
+
+        cookies[cookie_name] = rand_code
+        cookies[cookie_name].path = meta.csrf_options['CSRF_COOKIE_PATH']
+        cookies[cookie_name].expires = expires_cookie
+
+        meta.cookies = cookies
+
+        return value_csrf
+
+    def generate_csrf_token(self, csrf_token_field):
+
+        meta = self.form_meta
+
+        request = meta.request
+        cookie_name = meta.csrf_options['CSRF_COOKIE_NAME'] + "_" + meta.form_name
+
+        if request.method in ['POST', 'HEAD', 'PUT']:
+            if request.cookies:
+                if cookie_name in request.cookies:
+                    return request.cookies[cookie_name]
+
+        value_csrf = self.create_session()
+        return value_csrf
+
+    def delete_session(self):
+
+        meta = self.form_meta
+        cookie_name = meta.csrf_options['CSRF_COOKIE_NAME'] + "_" + meta.form_name
+
+        if not meta.csrf_options['CSRF_COOKIE_DOMAIN'] or meta.csrf_options['CSRF_COOKIE_DOMAIN'] == "":
+            domain = meta.request.host.split(':')[0]
+        else:
+            domain = meta.csrf_options['CSRF_COOKIE_DOMAIN']
+
+        cookies_options = {
+            'HTTP_COOKIE_DOMAIN': domain,
+            'HTTP_COOKIE_SECURE': meta.csrf_options['CSRF_COOKIE_SECURE'],
+            'HTTP_COOKIE_HTTPONLY': meta.csrf_options['CSRF_COOKIE_HTTPONLY']
+        }
+
+        cookies = meta.cookies
+        if not cookies:
+            cookies = CookieManager(options=cookies_options)
+
+        del cookies[cookie_name]
+
+        meta.cookies = cookies
+
+    def validate_csrf_token(self, form, field):
+
+        meta = self.form_meta
+
+        time_limit = meta.csrf_options['CSRF_TIME_LIMIT']
+        csrf_secret = meta.csrf_options['CSRF_SECRET']
+        cookie_name = meta.csrf_options['CSRF_COOKIE_NAME'] + "_" + meta.form_name
+        request = meta.request
+
+        if not field.data or '##' not in field.data:
+            raise ValidationError(field.gettext('CSRF token missing'))
+
+        expires, hmac_csrf = field.data.split('##', 1)
+
+        check_val = (str(request.cookies[cookie_name]) + expires).encode('utf8')
+
+        hmac_compare = hmac.new(csrf_secret, check_val, digestmod=sha1)
+        if hmac_compare.hexdigest() != hmac_csrf:
+            raise ValidationError(field.gettext('CSRF failed'))
+
+        if time_limit:
+            now_formatted = self.now().strftime(self.TIME_FORMAT)
+            if now_formatted > expires:
+                raise ValidationError(field.gettext('CSRF token expired'))
+
+    def now(self):
+        """
+        Get the current time. Used for test mocking/overriding mainly.
+        """
+        return datetime.datetime.now()
+
+
+class KairaForm(KairaFormBase):
+
+    class Meta:
+        csrf = True
+        csrf_class = KSessionCSRF
+        csrf_options = {
+            'CSRF_COOKIE_DOMAIN': '',
+            'CSRF_COOKIE_SECURE': False,
+            'CSRF_COOKIE_HTTPONLY': True,
+            'CSRF_COOKIE_EXPIRE': 3600,
+            'CSRF_COOKIE_PATH': '/',
+            'CSRF_COOKIE_NAME': 'csrf',
+            'CSRF_SECRET': b'APj00jpfj8Gx1SjnyLxwBBSQfnQ9DJYe0Cm',
+            'CSRF_TIME_LIMIT': timedelta(minutes=60)
+            }
+
+    def __init__(self, request, cookies=None, csrf_options=None, form_name='my_form', **kwargs):
+
+        self.request = request
+        self.csrf_options = csrf_options
+        self.cookies = cookies
+        if not csrf_options:
+            csrf_options = {
+                'CSRF_COOKIE_DOMAIN': '',
+                'CSRF_COOKIE_SECURE': False,
+                'CSRF_COOKIE_HTTPONLY': True,
+                'CSRF_COOKIE_EXPIRE': 3600,
+                'CSRF_COOKIE_PATH': '/',
+                'CSRF_COOKIE_NAME': 'csrf',
+                'CSRF_SECRET': b'APj00jpfj8Gx1SjnyLxwBBSQfnQ9DJYe0Cm',
+                'CSRF_TIME_LIMIT': timedelta(minutes=60)
+            }
+
+        super().__init__(request=request, meta=dict(cookies=cookies,
+                                                    request=request,
+                                                    csrf_options=csrf_options,
+                                                    form_name=form_name), **kwargs)
+
+    def render_boostrap_form(self, path='/', send_caption='Send'):
+        """ Render form"""
+
+        r_fields = self.render_boostrap_fields()
+        if self.csrf_token.errors:
+            csrf_errors = self.csrf_token.errors
+        else:
+            csrf_errors = ''
+        submit = '<button type="submit"> {send_caption} </button>'.format(send_caption=send_caption)
+        xml = '<form method="POST" enctype="multipart/form-data" action="{path}">' \
+              '{csrf_token} {csrf_errors}' \
+              '{fields} ' \
+              '{submit}' \
+              '</form>'\
+            .format(path=path,
+                    fields=r_fields,
+                    submit=submit,
+                    csrf_token=self.csrf_token,
+                    csrf_errors=csrf_errors)
         return xml
 
 
@@ -123,3 +337,4 @@ def serialize(obj):
 
 def deserialize(s):
     return pickle.loads(base64.b64decode(s))
+
